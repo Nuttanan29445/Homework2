41febc61bd67182f70b4578c837fccb87a98b7c6
NUTTANAN RUANGPHANICH <63010320@KMITL.AC.TH>
2020-08-15 20:12:58 +0700

    Add project files.

---
+++ Homework2/Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num,sum=0;
+	printf("Please input number:");
+	scanf_s("%d", &num);
+	for (int i = 0; i <= num; i++) 
+	{
+		sum += i;
+	}
+	printf("Summary number of 1 to %d = %d",num, sum);
+}
